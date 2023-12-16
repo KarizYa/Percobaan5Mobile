2bb36791b6935961cbb106d65f4bbbdf81951d8a
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2023-12-16 22:47:16 +0800

    second commit

--- app/src/main/java/com/dimaz/f55122003_perc5/ListHeroAdapter.java
+++ app/src/main/java/com/dimaz/f55122003_perc5/ListHeroAdapter.java
@@ -1,66 +1,70 @@
-package com.dimaz.f55122003_perc5;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+    package com.dimaz.f55122003_perc5;
+    import android.content.Intent;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+    import androidx.annotation.NonNull;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+    import java.util.ArrayList;
 
-public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
-    private ArrayList<Hero> ListHero;
+    public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
+        private ArrayList<Hero> ListHero;
 
-    private OnItemCLickCallback onItemClickCallback;
-    public void setOnItemClickCallback(OnItemCLickCallback onItemClickCallback) {
-        this.onItemClickCallback = onItemClickCallback;
-    }
-    public ListHeroAdapter(ArrayList<Hero> List) {
-        this.ListHero = List;
-    }
+        private OnItemCLickCallback onItemClickCallback;
+        public void setOnItemClickCallback(OnItemCLickCallback onItemClickCallback) {
+            this.onItemClickCallback = onItemClickCallback;
+        }
+        public ListHeroAdapter(ArrayList<Hero> List) {
+            this.ListHero = List;
+        }
 
-    @NonNull
-    @Override
-    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate
-                (R.layout.item_row_hero, parent, false);
-        return new ListViewHolder(view);
-    }
+        @NonNull
+        @Override
+        public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate
+                    (R.layout.item_row_hero, parent, false);
+            return new ListViewHolder(view);
+        }
 
-    @Override
-    public void onBindViewHolder(@NonNull final ListViewHolder holder, int position) {
-        Hero hero = ListHero.get(position);
-        holder.imgPhoto.setImageResource(hero.getPhoto());
-        holder.tvName.setText(hero.getName());
-        holder.tvDescription.setText(hero.getDescription());
-        holder.itemView.setOnClickListener(v -> {
-            Toast.makeText(holder.itemView.getContext(), "Kamu memilih " +
-                    ListHero.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
-        });
-        holder.itemView.setOnClickListener(v -> onItemClickCallback.onItemClicked(ListHero.get(holder.getAdapterPosition())));
-    }
-    public interface OnItemCLickCallback {
-        void onItemClicked(Hero data);
-    }
+        @Override
+        public void onBindViewHolder(@NonNull final ListViewHolder holder, int position) {
+            Hero hero = ListHero.get(position);
+            holder.imgPhoto.setImageResource(hero.getPhoto());
+            holder.tvName.setText(hero.getName());
+            holder.tvDescription.setText(hero.getDescription());
+            holder.itemView.setOnClickListener(v -> {
+                Hero selectHero = ListHero.get(holder.getAdapterPosition());
+                Intent intent = new Intent(holder.itemView.getContext(), activityHero.class);
+                intent.putExtra("HERO_PHOTO", hero.getPhoto());
+                intent.putExtra("HERO_NAME", hero.getName());
+                intent.putExtra("HERO_DESCRIPTION", hero.getDescription());
+                holder.itemView.getContext().startActivity(intent);
+            });
+        }
+        public interface OnItemCLickCallback {
+            void onItemClicked(Hero data);
+        }
 
 
-    @Override
-    public int getItemCount() {
-        return ListHero.size();
-    }
+        @Override
+        public int getItemCount() {
+            return ListHero.size();
+        }
 
-    public class ListViewHolder extends RecyclerView.ViewHolder {
-        ImageView imgPhoto;
-        TextView tvName, tvDescription;
+        public class ListViewHolder extends RecyclerView.ViewHolder {
+            ImageView imgPhoto;
+            TextView tvName, tvDescription;
 
-        public ListViewHolder(View itemView) {
-            super(itemView);
-            imgPhoto = itemView.findViewById(R.id.img_item_photo);
-            tvName = itemView.findViewById(R.id.tv_item_name);
-            tvDescription = itemView.findViewById(R.id.tv_item_description);
+            public ListViewHolder(View itemView) {
+                super(itemView);
+                imgPhoto = itemView.findViewById(R.id.img_item_photo);
+                tvName = itemView.findViewById(R.id.tv_item_name);
+                tvDescription = itemView.findViewById(R.id.tv_item_description);
+            }
         }
     }
-}
---
+++ app/src/main/java/com/dimaz/f55122003_perc5/activityHero.java
@@ -0,0 +1,26 @@
+package com.dimaz.f55122003_perc5;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activityHero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heroactivity);
+        int photoResource = getIntent().getIntExtra("HERO_PHOTO", 0);
+        String name = getIntent().getStringExtra("HERO_NAME");
+        String description = getIntent().getStringExtra("HERO_DESCRIPTION");
+        ImageView imageView = findViewById(R.id.img_item_photo);
+        TextView nameTextView = findViewById(R.id.tv_item_name);
+        TextView descriptiomTextView = findViewById(R.id.tv_item_description);
+        imageView.setImageResource(photoResource);
+        nameTextView.setText(name);
+        descriptiomTextView.setText(description);
+    }
+
+}
